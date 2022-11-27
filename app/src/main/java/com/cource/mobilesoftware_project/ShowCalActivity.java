6d9a80b5b56459f665787495224088048d6acf29
Dongja <jdh981118@naver.com>
2022-11-27 18:01:50 +0900

    actionbar : create up(뒤로가기) 생성

--- app/src/main/java/com/cource/mobilesoftware_project/ShowCalActivity.java
+++ app/src/main/java/com/cource/mobilesoftware_project/ShowCalActivity.java
@@ -7,6 +7,7 @@ import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
 import android.widget.TextView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ShowCalActivity extends AppCompatActivity {
@@ -16,7 +17,7 @@ public class ShowCalActivity extends AppCompatActivity {
         setContentView(R.layout.show_cal_activity);
 
         //글자 색 일부 변경
-        TextView function_text = (TextView) findViewById(R.id.showCal_explain); //텍스트 변수 선언
+        TextView function_text = findViewById(R.id.showCal_explain); //텍스트 변수 선언
 
         String content = function_text.getText().toString(); //텍스트 가져옴.
         SpannableString spannableString = new SpannableString(content); //객체 생성
@@ -27,5 +28,15 @@ public class ShowCalActivity extends AppCompatActivity {
 
         spannableString.setSpan(new ForegroundColorSpan(Color.parseColor("#EC7357")), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         function_text.setText(spannableString);
+
+        if(getSupportActionBar()!=null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
     }
 }
